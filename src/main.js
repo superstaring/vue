8a93b7d7b96ece79d5a71af13fc2fe13ea5cda1b
1690133293@qq.com <1690133293@qq.com>
2019-11-19 14:11:59 +0800

    注释

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
@@ -7,13 +9,11 @@ import router from './router'
 import VueResource from 'vue-resource'
 import axios from 'axios'
 
-
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
 
 Vue.prototype.$axios = axios // 全局定义axios
-
 
 /* eslint-disable no-new */
 new Vue({
